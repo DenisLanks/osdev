174fe09ee0e6cf6b5491d76ff0b9f86d75dfd428
Denis Lanks <denislanks@hotmail.com>
2020-07-21 18:32:48 -0300

    carregando usando o CHS

--- src/assembly/mbr.asm
+++ src/assembly/mbr.asm
@@ -1,7 +1,20 @@
 [ORG 0x7C00]
-mov ax, 0x4F02
-mov bx, 0x0115
-int 0x10
+mov ah, 02
+mov al, 01
+;CX := ( ( cylinder and 255 ) shl 8 ) or ( ( cylinder and 768 ) shr 2 ) or sector;
+xor cx, cx
+or cx, 02
+mov dh, 0
+mov bx, 0x500
+mov es, bx
+xor bx, bx
+int 13h
+
+cmp ah, 0
+je 0x500
+hang:
+    jmp hang
+
 times 510 -  ($-$$) db 0
 db 0x55
 db 0xAA
