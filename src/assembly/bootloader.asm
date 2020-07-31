bb699f31fcb5e742f46e22008287bb4312ab1e46
Denis Lanks <denislanks@hotmail.com>
2020-07-30 22:35:19 -0400

    Configurando porta serial

--- src/assembly/bootloader.asm
+++ src/assembly/bootloader.asm
@@ -3,5 +3,15 @@ mov ax, 0x4F02
 mov bx, 0x0115
 int 0x10
 
+push word 0x3F8
+call initSerial
+
+mov dx, 0x3F8
+xor ax, ax
+mov al, "Y"
+out dx, al
+
 hang:
     jmp hang
+
+%include "src/assembly/serial.asm"
